33211143771dad6442875f716d4577796bb03c86
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-07-13 07:18:35 +0300

    Initial Commit

---
+++ WorkingWithArrays/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  WorkingWithArrays
+//
+//  Created by Panayotis Matsinopoulos on 13/7/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
