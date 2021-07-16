76c066a38ed2e4f9ef8c4bc287a78cdc7d60027f
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-07-16 21:00:47 +0300

    Sorting mutable array with custom comparator

--- WorkingWithArrays/main.c
+++ WorkingWithArrays/main.c
@@ -8,6 +8,34 @@
 #include <stdio.h>
 #include <CoreFoundation/CoreFoundation.h>
 
+CFComparisonResult CustomersComparator (const void *val1, const void *val2, void *context) {
+  CFStringRef customer1 = (CFStringRef)val1;
+  CFStringRef customer2 = (CFStringRef)val2;
+  
+  CFArrayRef customer1Parts = CFStringCreateArrayBySeparatingStrings(kCFAllocatorDefault,
+                                                                     customer1,
+                                                                     CFSTR(" "));
+  CFStringRef customer1LastName = CFArrayGetValueAtIndex(customer1Parts, 1);
+  CFStringRef customer1FirstName = CFArrayGetValueAtIndex(customer1Parts, 0);
+
+  CFArrayRef customer2Parts = CFStringCreateArrayBySeparatingStrings(kCFAllocatorDefault,
+                                                                     customer2,
+                                                                     CFSTR(" "));
+  CFStringRef customer2LastName = CFArrayGetValueAtIndex(customer2Parts, 1);
+  CFStringRef customer2FirstName = CFArrayGetValueAtIndex(customer2Parts, 0);
+
+  CFComparisonResult result = CFStringCompare(customer1LastName, customer2LastName, 0);
+  
+  if ( result == kCFCompareEqualTo) {
+    result = CFStringCompare(customer1FirstName, customer2FirstName, 0);
+  }
+    
+  CFRelease(customer1Parts);
+  CFRelease(customer2Parts);
+  
+  return result;
+}
+
 int main(int argc, const char * argv[]) {
   CFMutableArrayRef cfCustomers = CFArrayCreateMutable(kCFAllocatorDefault,
                                                        0,
@@ -27,7 +55,7 @@ int main(int argc, const char * argv[]) {
   
   CFArraySortValues(cfCustomers,
                     CFRangeMake(0, CFArrayGetCount(cfCustomers)),
-                    (CFComparatorFunction)CFStringCompare, NULL);
+                    (CFComparatorFunction)CustomersComparator, NULL);
 
   printf("After sorting:\n");
   CFShow(cfCustomers);
